0c799ceeba9b013879dbf898b9de38f291a57eef
Varun Sukheja <varun.sukheja@here.com>
2021-08-01 00:10:07 +0530

    feat: setup elsint with prettier and resolved lint issues

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 import { createStore, StoreProvider } from 'easy-peasy';
+
+import reportWebVitals from './reportWebVitals';
+import App from './App';
 import stores from './store';
+
+import './index.css';
 
 const store = createStore(stores);
 
