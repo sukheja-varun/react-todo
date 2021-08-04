694798f8735b47dbd9ce17ab1d570c94cc6d3f44
Varun Sukheja <varun.sukheja@here.com>
2021-08-04 23:59:35 +0530

    feat: added react routing

---
+++ src/pages/Todos/index.tsx
@@ -0,0 +1,10 @@
+import React, { memo } from 'react';
+
+// import styles from'./index.module.scss';
+
+interface TodosProps {}
+const Todos: React.FC<TodosProps> = () => {
+  // const {} = props;
+  return <div>Todos Page</div>;
+};
+export default memo(Todos);
